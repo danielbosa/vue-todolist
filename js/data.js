f17368ef60c06a4aa69a407b4cc8fb2ae326de60
danielbosa <danielbosa95@gmail.com>
2024-04-02 16:11:15 +0200

    Starting

---
+++ js/data.js
@@ -0,0 +1,22 @@
+export const todo = [
+    {
+        id: 1,
+        text: 'costume da bagno',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'carta di credito',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'paletta e secchiello',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'crema solare',
+        done: false
+    },
+]
